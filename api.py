1908770744a43db19a7725edfd8d09633b2d9162
Sanjin84 <ozrobotix@gmail.com>
2023-12-08 15:35:56 +1100

    updated validation to accept running code without errors

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, status
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, StrictStr, StrictInt, Field, field_validator, ValidationError, StrictBool
+from pydantic_core.core_schema import FieldValidationInfo
+from typing import Optional
 import uvicorn
-from pydantic import validator, Field
 import requests
 from dotenv import load_dotenv
 import os
@@ -18,20 +19,26 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
-
+#test2 = CodeHintResponse(error_free=True, error_line=5, small_hint="", big_hint="")
 class CodeHintResponse(BaseModel):
-    error_free: bool
-    error_line: int = Field(default=None, alias='error_line')
-    small_hint: str
-    big_hint: str
+    error_free: StrictBool
+    error_line: Optional[int] = Field(None, ge=1, description="The line number of the first error")
+    small_hint: StrictStr = Field("", max_length=50, description="A small hint")
+    big_hint: StrictStr = Field("", max_length=200, description="A big hint")
 
-    @validator('error_line', pre=True, always=True)
-    def validate_error_line(cls, v, values, **kwargs):
-        if values.get('error_free') is True and v is not None:
-            raise ValueError("error_line must be None if error_free is True")
-        if values.get('error_free') is False and (v is None or v <= 0):
+    @field_validator('error_line')
+    def validate_error_line(cls, er_line, info: FieldValidationInfo):
+        if len(info.data) == 0:
+            return
+        #view the information contained FieldValidationInfo
+        print("info.data", info.data)
+        error_free = info.data['error_free']
+        if error_free and er_line != None:
+            raise ValueError("CONTRADICTION error_line must be None if error_free is True")
+        if error_free == False and (er_line == None or er_line < 1):
             raise ValueError("error_line must be an integer > 0 if error_free is False")
-        return v
+        return er_line
+    
     
 class CodeRequest(BaseModel):
     code: str
@@ -49,7 +56,7 @@ def get_code_hints_from_openai(code: str):
         "messages": [
             {
                 "role": "system",
-                "content": "You are an assistant. Your task is to analyze Python code and provide a JSON response with the following structure: {\"error_free\": BOOLEAN, \"error_line\": INT or None, \"small_hint\": STR (maximum of 8 words), \"big_hint\": STR (up to 30 words)}. You should not make up any additional values or provide any information not requested."
+                "content": "You are an assistant. Your task is to analyze Python code and provide a JSON response with the following structure: {\"error_free\": BOOLEAN, \"error_line\": INT (None if error_free is true), \"small_hint\": STR (maximum of 8 words), \"big_hint\": STR (up to 30 words)}. You should not make up any additional values or provide any information not requested."
             },
             {
                 "role": "user",
@@ -70,7 +77,6 @@ def process_code_snippet(code_snippet: str):
     
     # Find the assistant's reply message containing the JSON object
     json_reply = next((msg for msg in messages if msg.get("message", {}).get("role") == "assistant"), None)
-    
     # Extract the JSON content from the reply
     if json_reply and json_reply.get("message", {}).get("content"):
         json_content = json.loads(json_reply["message"]["content"])
@@ -78,6 +84,12 @@ def process_code_snippet(code_snippet: str):
         error_line = json_content.get("error_line", None)
         small_hint = json_content.get("small_hint", "")
         big_hint = json_content.get("big_hint", "")
+        print("error_free", error_free, type(error_free))
+        print("error_line", error_line, type(error_line))
+        print("small_hint", small_hint , type(small_hint))
+        print("big_hint", big_hint , type(big_hint))
+        print('----------------------------------')
+
     else:
         # Fallback values if the parsing fails
         error_free = True
@@ -85,15 +97,20 @@ def process_code_snippet(code_snippet: str):
         small_hint = "Could not get hint"
         big_hint = "Could not get hint"
 
+    if error_free:
+        error_line = None
+        small_hint = ""
+        big_hint = ""
+
     hint_response = CodeHintResponse(
         error_free=error_free,
         error_line=error_line,
         small_hint=small_hint,
         big_hint=big_hint
     )
-
-    # Return the JSON representation
-    return hint_response.json(by_alias=True)
+    print("after hint_response")
+    # Return the JSON representation using model dump
+    return hint_response.model_dump()
 
 @app.get("/")
 async def root():
@@ -102,21 +119,9 @@ async def root():
 
 @app.post("/get_code_hints")
 async def get_code_hints(code_request: CodeRequest):
-    try:
-        return process_code_snippet(code_request.code)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    return process_code_snippet(code_request.code)
+
     
-@app.post("/get_code_hints_form")
-async def get_code_hints(request: Request):
-    form_data = await request.form()
-    code_snippet = form_data.get("code")
-
-    try:
-        return process_code_snippet(code_snippet)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
 # Run the application
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
